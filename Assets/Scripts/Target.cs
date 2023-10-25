b847c04e1a772b4e84ef0cd91558784f0854ebb2
Etheratic <136394417+Etheratic@users.noreply.github.com>
2023-10-25 21:16:51 +1100

    Enums, Coroutines, Switch

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,9 +5,75 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public int health = 3;
-    
+    public float speed = 10f;
+  
+    public float scalefactor = 1;
+    public sizes sizes;
+    public TargetManager _TM;
 
-   
+    public Transform moveToPos;
+
+
+
+
+    private void Start()
+    {
+        _TM = FindObjectOfType<TargetManager>();
+        SetUp();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            ChangeSize();
+        }
+    }
+    public void SetUp()
+    {
+        switch(sizes)
+        {
+            case sizes.Small:
+                health = 1;
+                gameObject.transform.localScale = Vector3.one * scalefactor * .5f;
+                speed = speed * 2;
+            break;
+            case sizes.Medium:
+                gameObject.transform.localScale = Vector3.one * scalefactor * 1f;
+                health = 3;
+               ;
+            break;
+            case sizes.Large:
+                health = 5;
+                speed = speed / 2;
+                gameObject.transform.localScale = Vector3.one * scalefactor * 3;
+                
+                break;
+        }
+
+        StartCoroutine(Move());
+    }
+
+    private void ChangeSize()
+    {
+        
+        gameObject.transform.localScale = Vector3.one * scalefactor * Random.Range(.5f, 10);
+    }
+
+    IEnumerator Move()
+    {
+        moveToPos = _TM.GetRandomSpawnpoint();
+        
+        while(Vector3.Distance(transform.position,moveToPos.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, moveToPos.position, Time.deltaTime * speed);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(3);
+        StartCoroutine(Move());
+    }
+
     public void CheckHealth()
     {
         if (health <= 0)
@@ -20,7 +86,7 @@ public class Target : MonoBehaviour
         health = health -1;
     }
 
-   
 
+   
 
 }
--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum sizes
+{
+    Small, Medium, Large
+}
+
 
 public class TargetManager : MonoBehaviour
 {
 
     public Transform[] spawnPoints;
     public List<GameObject> targets;
+    public int numberOfTargets = 10;
+
+    private Target Target;
 
     void Start()
     {
+        for(int i = 0; i < numberOfTargets; i++)
+        {
+            SpawnAtRandom();
+        }
+
         
     }
 
+    public Transform GetRandomSpawnpoint() 
+    { 
+        return spawnPoints[Random.Range(0, spawnPoints.Length)];
+    
+    }
     
     void Update()
     {
@@ -20,8 +38,13 @@ public class TargetManager : MonoBehaviour
         {
             SpawnAtRandom();
         }
+
+      
+
     }
 
+
+ 
     private void SpawnAtRandom()
     {
         if (targets.Count == 0)
@@ -31,9 +54,12 @@ public class TargetManager : MonoBehaviour
         int rndTarget = Random.Range(0, targets.Count - 1);
         int rndSpawnPoint = Random.Range(0, spawnPoints.Length - 1);
 
+        
+
         //spawn in the enemies
         GameObject target = Instantiate(targets[rndTarget], spawnPoints[rndSpawnPoint].position, spawnPoints[rndSpawnPoint].rotation);
         targets.Add(target);
+
     }
 
 }
